defd83e8f6c705de2d751ad74cfec3c87f61c0d8
pzlabuda <pzlabuda@users.noreply.github.com>
2022-12-01 15:57:31 +0100

    Upgraded okhttp

--- app/src/main/java/acr/browser/lightning/settings/fragment/AdBlockSettingsFragment.kt
+++ app/src/main/java/acr/browser/lightning/settings/fragment/AdBlockSettingsFragment.kt
@@ -23,8 +23,10 @@ import io.reactivex.Scheduler
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
 import io.reactivex.rxkotlin.subscribeBy
-import okhttp3.HttpUrl
-import okio.Okio
+import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import okio.buffer
+import okio.sink
+import okio.source
 import java.io.File
 import java.io.IOException
 import javax.inject.Inject
@@ -141,7 +143,7 @@ class AdBlockSettingsFragment : AbstractSettingsFragment() {
             currentText = userPreferences.hostsRemoteFile,
             action = R.string.action_ok,
             textInputListener = {
-                val url = HttpUrl.parse(it)
+                val url = it.toHttpUrlOrNull()
                     ?: return@showEditText run { activity?.toast(R.string.problem_download) }
                 userPreferences.hostsSource = HostsSourceType.Remote(url).toPreferenceIndex()
                 userPreferences.hostsRemoteFile = it
@@ -189,8 +191,8 @@ class AdBlockSettingsFragment : AbstractSettingsFragment() {
         try {
             val outputFile = File(externalFilesDir, AD_HOSTS_FILE)
 
-            val input = Okio.source(inputStream)
-            val output = Okio.buffer(Okio.sink(outputFile))
+            val input = inputStream.source()
+            val output = outputFile.sink().buffer()
             output.writeAll(input)
             return@create it.onSuccess(outputFile)
         } catch (exception: IOException) {
