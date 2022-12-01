996043b395d5676f13449d8f36c8125c1a183b20
pzlabuda <pzlabuda@users.noreply.github.com>
2022-12-01 16:22:16 +0100

    Updated User-Agents

--- app/src/main/java/acr/browser/lightning/constant/Constants.kt
+++ app/src/main/java/acr/browser/lightning/constant/Constants.kt
@@ -6,8 +6,8 @@
 package acr.browser.lightning.constant
 
 // Hardcoded user agents
-const val DESKTOP_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
-const val MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 10; Pixel Build/QP1A.190711.019; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36"
+const val DESKTOP_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
+const val MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.88 Mobile Safari/537.36"
 
 // URL Schemes
 const val HTTP = "http://"
